79aa0efbb76874055c16dbd08e22e34059792944
YUMMY-WHISKY <mozixi86@gmail.com>
2024-10-31 22:36:23 +0800

    Add files via upload

---
+++ 百元买鸡.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int i, j, k;//母i公j小k，数量
+	for (i = 1; i <= 19; i++)
+	for (j = 1; j <= 31; j++)
+	for (k = 1; k <= 98; k++)
+	if(i + j + k ==100 && 5 * i + 3 * j + 0.5 * k ==100)
+	printf("%d %d %d\n",i, j, k);
+	return 0;
+}
